eb6b9648aa9bfb275c65e89e05739b9db7fff02f
Rune Ovlien Rakeie <rune.rakeie@tietoevry.com>
2025-03-22 22:42:04 +0100

    Add: Restore infra

---
+++ tf_restore/data-sources.tf
@@ -0,0 +1,30 @@
+#######################
+### RESOURCE GROUPS ###
+#######################
+data "azurerm_resource_group" "fc_cap_rg" {
+  name = "datarunefabric-wus"
+}
+#######################
+### FABRIC CAPACITY ###
+#######################
+data "fabric_capacity" "capacity" {
+  display_name = "fc${var.solution_name_restored}"
+}
+#######################
+### Entra ID Group  ###
+#######################
+data "azuread_group" "fabric_ws_contributors" {
+  display_name = "Terraform Demo - Workspace Contributors"
+}
+#######################
+###  Entra ID User  ###
+#######################
+data "azuread_user" "admin" {
+  user_principal_name = var.admin_user
+}
+###################################
+### Entra ID Service Principal  ###
+###################################
+data "azuread_service_principal" "tf_sp" {
+  display_name = "pcs-s-sp-terraform"
+}
